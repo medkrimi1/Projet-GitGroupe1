da489db250142d89172b42454faf95f9c1346924
Fatmabidi1 <fatma.abidi@esprit.tn>
2023-04-09 02:01:15 +0100

    add function getCategorieProduit()

--- src/main/java/com/esprit/examen/controllers/ProjectController.java
+++ src/main/java/com/esprit/examen/controllers/ProjectController.java
@@ -1,4 +1,27 @@
 package com.esprit.examen.controllers;
 
+import com.esprit.examen.entities.CategorieProduit;
+import com.esprit.examen.services.ICategorieProduitService;
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+@Api(tags = "Gestion des categories Produit")
+@RequestMapping("/categorieProduit")
+@RequiredArgsConstructor
+
 public class ProjectController {
+    //fatma : je suis entrain d'ajouter les fonctions de categorieProductController
+    private final ICategorieProduitService categorieProduitService;
+
+    @GetMapping
+    public List<CategorieProduit> getCategorieProduit() {
+        List<CategorieProduit> list = categorieProduitService.retrieveAllCategorieProduits();
+        return list;
+    }
+
 }
