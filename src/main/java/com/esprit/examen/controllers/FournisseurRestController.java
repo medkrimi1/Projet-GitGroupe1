18742f73478adc6df01ecd681243cecd0ed605c4
SarraAROURI <sarraarouri4@gmail.com>
2023-04-15 02:25:59 +0100

    Fournisseur

--- src/main/java/com/esprit/examen/controllers/FournisseurRestController.java
+++ src/main/java/com/esprit/examen/controllers/FournisseurRestController.java
@@ -52,7 +52,7 @@ public class FournisseurRestController {
 	}
 
 		@PutMapping(value = "/assignSecteurActiviteToFournisseur/{idSecteurActivite}/{idFournisseur}")
-		public void assignProduitToStock(@PathVariable("idSecteurActivite") Long idSecteurActivite, @PathVariable("idFournisseur") Long idFournisseur) {
+		public void assignProduitTo(@PathVariable("idSecteurActivite") Long idSecteurActivite, @PathVariable("idFournisseur") Long idFournisseur) {
 			fournisseurService.assignSecteurActiviteToFournisseur(idSecteurActivite, idFournisseur);
 		}
 
